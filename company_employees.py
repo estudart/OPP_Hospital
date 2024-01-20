fe0c910e8550beb05ce4f3fb327c99326df7cd32
estudart <erico.studartdf@gmail.com>
2024-01-20 12:37:08 -0300

    update

--- company_employees.py
+++ company_employees.py
@@ -8,11 +8,9 @@ class Employee(ABC):
         self.role = role
         self.active = active
         self.hospital = hospital
-    
     @abstractmethod
     def __str__(self):
        pass
-
     @abstractmethod
     def inactive_employee(self):
         pass
@@ -22,29 +20,24 @@ class Doctor(Employee):
     def __init__(self, name: str, age: int, salary: float, especialization: str):
         super().__init__(name, age, salary, role="Doctor", active=True, hospital=None)
         self.especialization = especialization
-
     def __str__(self):
         if self.hospital:
             return f"{self.role} {self.name}, Hopistal: {self.hospital}"
         else:
             return f"{self.name}, Hopistal: Not registered"
-    
     def inactive_employee(self):
         self.active = False
         return
-    
 
 class Nurse(Employee):
     def __init__(self, name: str, age: int, salary: float, department: str):
         super().__init__(name, age, salary, role="Nurse", active=True, hospital=None)
         self.department = department
-    
     def __str__(self):
         if self.hospital:
             return f"{self.name}, Hopistal: {self.hospital} "
         else:
             return f"{self.name}, Hopistal: Not registered"
-    
     def inactive_employee(self):
         self.active = False
         return
@@ -53,15 +46,12 @@ class Hospital:
     def __init__(self, name: str):
         self.name = name
         self.employees = []
-    
     def hire_employee(self, employee):
         employee.hospital = self.name
         self.employees.append(employee.name)
         print(f"{employee.name} hired at {self.name}")
-
     def get_employees(self):
         return print(f"{self.employees}")
-    
     def fire_employee(self, employee):
        for i, emp in enumerate(self.employees):
            if emp == employee.name:
